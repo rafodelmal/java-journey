c7a888ec8eeaaf686959952173ab683245329c0b
Jose Rafael Arrieta Dominguez <rafo.reloaded@gmail.com>
2023-12-02 08:33:48 -0500

    Add Spring module, Add docker compose, setup maven pluging to pom.xml

---
+++ springboot/src/main/java/co/com/joserafael/java/Main.java
@@ -0,0 +1,24 @@
+package co.com.joserafael.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+        // Press Shift+F9 to start debugging your code. We have set one breakpoint
+        // for you, but you can always add more by pressing Ctrl+F8.
+
+        SpringApplication.run(Main.class, args);
+
+    }
+}
